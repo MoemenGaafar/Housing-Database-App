38f150ad0ed506f38731ca13f7d9d3c575309eb1
Moemen Gaafar <201700873@zewailcity.edu.eg>
2020-05-08 18:39:08 +0200

    StoredProc

--- StoredProcedures.cs
+++ StoredProcedures.cs
@@ -8,9 +8,14 @@ namespace DBapplication
     public class StoredProcedures
     {
         public static string CheckPassword_Citizen = "CheckPassword_Citizen";
+        //INPUT: National ID, Password - OUTPUT: All
         public static string CheckPassword_Employee = "CheckPassword_Employee";
+        //INPUT: National ID, Password - OUTPUT: All
         public static string CheckPassword_Company = "CheckPassword_Company";
+        //INPUT: Company ID, Password - OUTPUT: All
         public static string CheckPassword_Bank = "CheckPassword_Bank";
+        //INPUT: Name, Password - OUTPUT: All
         public static string CheckPassword_Admin = "CheckPassword_Admin";
+        //INPUT: Username, Password - OUTPUT: All
     }
 }
